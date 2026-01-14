9a1ce059d0ccd2529a0912b623f9ee866d17e52d
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-01-14 13:19:14 -0600

    Create main.cpp

---
+++ 012_Loops/81_LoopWithOutput/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+
+    /*
+    for(size_t i{0} ; i < 100000 ; ++i){
+        //std::cout << "i : " << i << std::endl;
+    }
+    std::cout << "Done!" << std::endl;
+    */
+
+   size_t i{0}; // Iterator
+
+   while(i < 100000){
+        //std::cout << "i : " << i << std::endl; 
+        ++i;     
+   }
+   std::cout << "Done!" << std::endl;
+   
+    return 0;
+}
+
+//if we use cout it will take much longer to excecute the program
