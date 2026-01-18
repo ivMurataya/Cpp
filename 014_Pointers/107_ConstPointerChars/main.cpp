407c9a90bcad2991da24a20a0e46741a2ad9e60f
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-01-17 23:26:44 -0600

    Create main.cpp

---
+++ 014_Pointers/107_ConstPointerChars/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+
+int main(){
+
+    //const  char * const   students [] {
+    const  char*    students[]{
+        "Daniel Gray",
+        "Tuna Philbe",
+        "The Rock",
+        "Kumar Singh",
+        "Sam Ali",
+        "Dave Mahoro"
+    };
+    
+
+    //Print out the students
+	std::cout << std::endl;
+    std::cout << "Printing out the students : " << std::endl;
+    for ( const char * student : students){
+        std::cout << student << std::endl;
+    }
+
+
+   // *students[0] = 'K';
+
+    //Can swap for new student though.
+    const char * new_student {"Bob the Great"};
+    students[0] = new_student; // Compiler error on const  char * const   students []
+
+
+   //Print out the students
+	std::cout << std::endl;
+    std::cout << "Printing out the students : " << std::endl;
+    for ( const char * student : students){
+        std::cout << student << std::endl;
+    }
+   
+    return 0;
+}
