d4214a18e05b84186a320eefa13c0017c2bb3b9a
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-01-17 21:46:15 -0600

    Create main.cpp

---
+++ 014_Pointers/104_CharPointers/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+int main(){
+
+    const char * message {"Hello World!"};
+    std::cout << "message : " << message << std::endl;
+
+    //*message = "B"; // Compiler error
+    std::cout << "*message : " << *message << std::endl;
+
+    //Allow users to modify the string
+    char message1[] {"Hello World!"};
+    message1[0] = 'B';
+    std::cout << "message1 : " << message1 << std::endl;
+    
+    return 0;
+}
+
+/*
+message : Hello World!
+*message : H
+message1 : Bello World!
+*/
