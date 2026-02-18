b7f362dba249b7389b32890a6cceefd3023038c6
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-02-18 16:34:19 -0500

    Add files via upload

---
+++ 017_Functions/152_MultipleFiles/compare.cpp
@@ -0,0 +1,12 @@
+
+int min( int a, int b){
+    if(a<b)
+        return a;
+    else
+        return b;
+}
+
+int multy(int a, int b) {
+    return a * b * 100;
+
+}
---
+++ 017_Functions/152_MultipleFiles/operations.h
@@ -0,0 +1,4 @@
+
+int incr_mult(int a, int b);
+
+int multy (int a, int b);
