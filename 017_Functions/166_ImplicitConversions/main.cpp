78b87d71c3cce07d20c85e1851850f4c7ebc5f9b
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-02-19 11:08:48 -0500

    Create main.cpp

---
+++ 017_Functions/166_ImplicitConversions/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+void print_sum(int a, int b) {
+
+	int sum = a + b;
+	std::cout << "sizeof(a) : " << sizeof(a) << std::endl;
+	std::cout << "sizeof(b) : " << sizeof(b) << std::endl;
+	std::cout << "sizeof(int) : " << sizeof(int) << std::endl;
+	std::cout << "sizeof(double) : " << sizeof(double) << std::endl;
+	std::cout << "sum : " << sum << std::endl;
+	std::cout << std::endl;
+}
+
+
+int main() {
+
+	double a{ 4.5 };
+	double b{ 7.8 };
+
+	print_sum(a, b);
+
+	return 0;
+
+}
+
+/*
+sizeof(a) : 4
+sizeof(b) : 4
+sizeof(int) : 4
+sizeof(double) : 8
+sum : 11
+*/
