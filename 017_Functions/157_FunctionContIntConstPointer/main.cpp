cdcf96a9ac29ed4c606a7b87df5a6f182e5c2941
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-02-19 08:41:22 -0500

    Create main.cpp

---
+++ 017_Functions/157_FunctionContIntConstPointer/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int dog_count{2};
+
+void say_age(const int* const a); // Declaration
+
+int main(){
+
+    int age{23}; // Local
+    std::cout << "age (before call) : " << age << "&age : " << &age << std::endl; //23
+    say_age(&age); // Argument
+    std::cout << "age (after call) : " << age << "&age : " << &age <<  std::endl; //24
+
+    return 0;
+}
+
+
+void say_age(const int* const a){ // Parameter
+    //++(*age); //
+    std::cout <<  "Hello , you are " << *a << " years old! &age : " << &a <<  std::endl;//24
+    //age = & dog_count;
+}
+
+
+/*
+
+age (before call) : 23        &age : 000000FCE12FFC64
+Hello , you are 23 years old! &age : 000000FCE12FFC40
+age (after call) : 23         &age : 000000FCE12FFC64
+
+*/
