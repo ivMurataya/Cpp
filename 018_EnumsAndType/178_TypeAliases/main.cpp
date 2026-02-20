9b98d5418c706185b9feab70d4fff26cfe312732
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-02-20 07:55:39 -0500

    Create main.cpp

---
+++ 018_EnumsAndType/178_TypeAliases/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+
+     using HugeInt = unsigned long long int; // Recommended in modern C++
+     //typedef unsigned long long int HugeInt; // Older C++ syntax for type aliases
+     HugeInt huge_number {123378997};
+
+     std::cout << "sizeof(unsigned long long int ) : " << sizeof(unsigned long long int) << std::endl;
+     std::cout << "sizeof(HugeInt) : " << sizeof(HugeInt) << std::endl;
+
+    std::cout << "huge_number : " << huge_number << std::endl;
+
+    return 0;
+}
+
+/*
+
+sizeof(unsigned long long int ) : 8
+sizeof(HugeInt) : 8
+huge_number : 123378997
+
+*/
