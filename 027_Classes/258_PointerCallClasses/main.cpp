e6a990064669465a007f2c1e65e1b8995c027bf4
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-03-18 11:40:00 -0400

    Create main.cpp

---
+++ 027_Classes/258_PointerCallClasses/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "cylinder.h"
+
+int main() {
+
+	Cylinder cylinder1(10, 10);
+
+	cylinder1.volume();
+
+	//Managing a stack object through pointers
+	Cylinder* p_cylinder1 = &cylinder1;
+
+	//std::cout << "volume : " << (*p_cylinder1).volume() << std::endl;
+	std::cout << "volume : " << p_cylinder1->volume() << std::endl;
+
+	//Create a cylinder heap object through the new operator
+	Cylinder* p_cylinder2 = new Cylinder(100, 2); // Heap
+
+	std::cout << "volume(clylinder2) : " << p_cylinder2->volume() << std::endl;
+	std::cout << "base_rad(cylinder2) : " << p_cylinder2->get_base_radius() << std::endl;
+
+
+
+
+	delete p_cylinder2;
+
+
+	return 0;
+}
+
+/*
+
+volume : 3141.59
+volume(clylinder2) : 62831.9
+base_rad(cylinder2) : 100
+
+*/
