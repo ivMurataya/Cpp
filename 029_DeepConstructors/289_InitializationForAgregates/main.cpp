28532c7fbcb412916d1e11edde8e4a68215dc9fe
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-04-10 16:05:54 -0400

    Create main.cpp

---
+++ 029_DeepConstructors/289_InitializationForAgregates/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+class Point{
+public:
+    Point(double x, double y) : m_x{x},m_y{y}{
+        std::cout << "Point constructor called" << std::endl;
+    }
+ private : 
+    double m_x ;
+    double m_y;
+};
+
+struct Person
+{
+    std::string m_name;
+    unsigned int m_age ;
+};
+
+
+
+int main(){
+
+    Point p1{10.9,39.3}; // Uniform initialization : () or {}
+
+    //Struct
+        // {}
+   // Person person1 {"Daniel",35};
+
+        //()
+    Person p2("John",41.7);
+
+    //This will allowr to the double number to be converted to int 
+    int students[] (1,2,3,4.7,5,6,7);
+
+    // Will throw an commiler error
+    int students2[]{1, 2, 3, 4.7, 5, 6, 7};
+
+    
+    return 0;
+}
