d5a3bfdf616cdbc5c3d5c7d2396d62427750acc4
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-05-20 15:07:25 -0400

    Create main.cpp

---
+++ 032_Namespaces/313_AnounymousNamespaces/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace{
+    double add(double a, double b); // Declaration
+} 
+
+
+int main(){
+
+    double result = add(10.2,20.2);
+    std::cout << "result : " << result << std::endl;
+   
+    return 0;
+}
+
+
+namespace{
+    double add(double a, double b){
+        return a + b;
+    }
+} 
+
+
+
+/*
+result : 30.4
+*/
