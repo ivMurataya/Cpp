6d76d5ebe94f85d604ed419d8a369090dddda636
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-05-21 09:00:04 -0400

    Add files via upload

---
+++ 033_ProgramWithMultipleFiles/319_DeclarationsAndDefinitions/person.h
@@ -0,0 +1,31 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <iostream>
+
+//Declaration and definition
+class Person{
+public : 
+    //Declaration for the constructor
+	Person(const std::string& names_param, int age_param);
+	
+	/*
+	void print_info()const{
+		std::cout << "name : " << full_name << " , age : " << age << std::endl;
+	}
+	*/ 
+
+	void print_info()const;
+
+
+private : 
+	std::string full_name;
+	int age;
+	
+public : 
+	//static variable declaration
+	static int person_count;
+};
+
+
+#endif //PERSON_H
