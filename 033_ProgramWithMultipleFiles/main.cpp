6f5324d33ee4db2779a041e719af14bc838d6b6b
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-05-21 08:17:10 -0400

    Create main.cpp

---
+++ 033_ProgramWithMultipleFiles/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "utilities.h"
+
+int main(){
+
+    double result = add(10.5,20.8);
+    std::cout << "result : " << result << std::endl;
+
+     result = multiply(10.5, 20.8);
+    std::cout << "result : " << result << std::endl;
+
+    
+    return 0;
+}
