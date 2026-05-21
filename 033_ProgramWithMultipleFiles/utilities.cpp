9047ce7420d56c5937e310e6d90b828c7605540e
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-05-21 08:20:09 -0400

    Add files via upload

---
+++ 033_ProgramWithMultipleFiles/utilities.cpp
@@ -0,0 +1,7 @@
+//Implementations
+// having the #include "utilities.h" is preferrable but not mandatory
+
+double add( double a, double b){
+	return a + b;
+}
+
---
+++ 033_ProgramWithMultipleFiles/utilities.h
@@ -0,0 +1,10 @@
+//Declarations
+//this will look at all the files for these 2 functions
+// you cound addd the #IFNDEF DEFINE
+#ifndef  UTILITIES_H
+#define UTILITIES_H
+
+double add(double a, double b);
+double multiply(double a, double b);
+#endif // ! 
+
