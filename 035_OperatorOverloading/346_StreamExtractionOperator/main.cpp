5cfa86c5afeffa26eb5c539ab88c8f28ba86d9c9
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-07-17 08:45:04 -0400

    Create main.cpp

---
+++ 035_OperatorOverloading/346_StreamExtractionOperator/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "point.h"
+
+int main(){
+
+    
+    Point p2;
+
+    std::cin >> p2;//
+
+    std::cout << "p2 : " << p2 << std::endl;
+   
+    return 0;
+}
+
+
+/*
+
+Please type in the coordinates for the point
+order [x,y], separated by spaces : 543.21 324.342
+p2 : Point [ x : 543.21, y : 324.342]
+
+*/
