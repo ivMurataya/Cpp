849dc8ea7110e35d5c488b3b20e3bb6d36fd5b7b
Ivan Murataya <104288536+ivMurataya@users.noreply.github.com>
2026-07-18 09:50:49 -0400

    Update main.cpp

--- 035_OperatorOverloading/349_CustomTypeConversions/main.cpp
+++ 035_OperatorOverloading/349_CustomTypeConversions/main.cpp
@@ -14,15 +14,25 @@ int main(){
 
     Number n1(22);
     Number n2(10);
+
+    std::cout << n1 << " - " << n2 << std::endl;
+    std::cout << (n1 + Number(100)) << std::endl;
+
+    //double sum0 = sum(n1, n2); // wont compile as sum expects a double not Number
+    double result = sum(static_cast<double>(n1),static_cast<double>(n2));
+    std::cout << "result : " << result <<  std::endl;
     
-    //double result = sum(static_cast<double>(n1),static_cast<double>(n2));
-    //std::cout << "result : " << result <<  std::endl;
+    //use_point(n2) // no conversion from Number to point, has to cast 
     use_point(static_cast<Point>(n1));
 
     return 0;
 }
 
 /*
+Number : [22] - Number : [10]
+Number : [122]
+result : 32
 Custom conversion from Number to Point
 Printing the point from use_point func : Point [ x : 22, y : 22]
+
 */
